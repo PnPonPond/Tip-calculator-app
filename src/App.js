ac66556386ffc031620d11b9bae1a1db924d70f4
PnPonPond <pnponpond@gmail.com>
2021-09-01 14:26:27 +0700

    responsive

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ function App() {
     } else {
       setCustom(false);
     }
-    console.log(tips);
     calcAll();
   };
 
@@ -59,27 +58,28 @@ function App() {
     setBillError(false);
     document.querySelector(".inputBill").value = 0;
     document.querySelector(".inputPeopleNumber").value = 0;
+    document.querySelector(".inputTipCustom").value = "";
   };
 
   return (
     <div className="App font-mono">
-      <div className="flex flex-col min-h-screen w-full	justify-center items-center mx-auto bg-indigo-200	">
-        <div className="text-lg mb-9 font-bold text-purple-800">
+      <div className="flex flex-col min-h-full sm:min-h-screen w-full	justify-center items-center mx-auto bg-Lightgrayishcyan	">
+        <div className="mt-9 text-xl mb-9  font-bold text-darkcyan sm:text-lg sm:mt-0">
           <div className="mb-1">S P L I</div>
           <div>T T E R</div>
         </div>
-        <div className="bg-white	p-8	rounded-3xl	w-1/2 min-w-max	text-sm">
-          <div className="flex items-center">
-            <div className="flex flex-col mr-10">
+        <div className="bg-White w-full	text-sm rounded-t-3xl p-8	sm:rounded-3xl sm:w-1/2 sm:min-w-max">
+          <div className="flex flex-col items-center sm:flex-row">
+            <div className="flex flex-col mb-8 sm:mb-0 sm:mr-10">
               <div className="flex justify-between items-center  mb-2">
-                <div>Bill</div>
+                <div className="text-darkcyan">Bill</div>
                 {billError ? (
-                  <div className="text-red-400">Can't be zero</div>
+                  <div className="text-red">Can't be zero</div>
                 ) : null}
               </div>
               <div
-                className={`flex justify-between bg-gray-100	px-4 py-2 rounded ${
-                  billError ? "border-2 border-red-400" : null
+                className={`flex justify-between border-2 border-transparent hover:border-cyan bg-LightGrayishcyan	px-4 py-2 rounded ${
+                  billError ? "border-2 border-red" : null
                 }`}
               >
                 <img className="h-full self-center" src="./icon-dollar.svg" />
@@ -92,12 +92,14 @@ function App() {
                   }}
                 />
               </div>
-              <h4 className="mt-5 text-left mb-2">Select Tip %</h4>
-              <div className="grid grid-cols-3 gap-4 text-xl">
+              <h4 className="mt-5 text-left mb-2 text-darkcyan">
+                Select Tip %
+              </h4>
+              <div className="grid grid-cols-2  gap-4 text-xl sm:grid-cols-3">
                 {btn.map((obj) => {
                   return (
                     <button
-                      className="bg-purple-800 text-white rounded py-2"
+                      className="bg-darkcyan text-LightGrayishcyan rounded py-2 hover:bg-Lightgrayishcyan hover:text-darkcyan"
                       onClick={() => handleTipBtn(obj)}
                     >
                       {obj * 100 + "%"}
@@ -107,21 +109,21 @@ function App() {
 
                 <input
                   type="number"
-                  className="bg-purple-800 text-white rounded w-24 text-center px-3 inputTipCustom"
+                  className="text-darkcyan rounded  text-center px-3 inputTipCustom border-2 border-transparent  bg-LightGrayishcyan hover:border-cyan focus:border-cyan sm:w-24"
                   placeholder="Custom"
                   onChange={(e) => handleTipCustom(e)}
                 />
               </div>
               <div>
                 <div className="flex justify-between items-center mt-5 mb-2">
-                  <div>Custom Number of People</div>
+                  <div className="text-darkcyan">Custom Number of People</div>
                   {personError ? (
-                    <div className="text-red-400">Can't be zero</div>
+                    <div className="text-red">Can't be zero</div>
                   ) : null}
                 </div>
                 <div
-                  className={`flex justify-between bg-gray-100	px-4 py-2 rounded ${
-                    personError ? "border-2 border-red-400" : null
+                  className={`flex justify-between border-2 border-transparent hover:border-cyan bg-LightGrayishcyan	px-4 py-2 rounded ${
+                    personError ? "border-2 border-red" : null
                   }`}
                 >
                   <img className="h-full self-center" src="./icon-person.svg" />
@@ -130,41 +132,40 @@ function App() {
                     type="number"
                     onChange={(e) => {
                       setPeople(e.target.value);
-                      // console.log(e.target.value);
                       calcAll();
                     }}
                   />
                 </div>
               </div>
             </div>
-            <div className="bg-purple-600	rounded-xl w-80 h-80 px-5 py-10 flex flex-col justify-between text-white">
+            <div className="bg-darkcyan	rounded-xl w-80 h-60 px-5 pt-10 pb-4 flex flex-col justify-between text-LightGrayishcyan sm:h-80 sm:pb-8">
               <div>
                 <div className="flex justify-between items-center">
                   <div className="text-left">
                     <div>Tip Amount</div>
-                    <div className="text-xs">/ person</div>
+                    <div className="text-xs text-Darkgrayishcyan">/ person</div>
                   </div>
-                  <div className="text-4xl">{tipAmount}$</div>
+                  <div className="text-4xl text-cyan">{tipAmount}$</div>
                 </div>
                 <div className="flex justify-between items-center mt-6 mb-">
                   <div className="text-left">
                     <div>Total</div>
-                    <div className="text-xs">/ person</div>
+                    <div className="text-xs text-Darkgrayishcyan">/ person</div>
                   </div>
-                  <div className="text-4xl	">{total}$</div>
+                  <div className="text-4xl	text-cyan">{total}$</div>
                 </div>
               </div>
               <button
-                className="w-full bg-purple-200	rounded py-2 btn-reset text-purple-600"
+                className="w-full text-lg hover:bg-Lightgrayishcyan	rounded py-2 btn-reset text-darkcyan bg-cyan"
                 onClick={() => handleResetBtn()}
               >
-                Reset
+                RESET
               </button>
             </div>
           </div>
         </div>
       </div>
-      <div class="attribution">
+      <div className="attribution text-xs">
         Challenge by{" "}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
       secondary: "#ffed4a",
       danger: "#e3342f",
     }),
+    colors: {
+      cyan: "hsl(172, 67%, 45%)",
+      darkcyan: "hsl(183, 100%, 15%)",
+      Darkgrayishcyan: "hsl(186, 14%, 43%)",
+      DarkGrayishcyan: "hsl(184, 14%, 56%)",
+      Lightgrayishcyan: "hsl(185, 41%, 84%)",
+      LightGrayishcyan: "hsl(189, 41%, 97%)",
+      White: "hsl(0, 0%, 100%)",
+      transparent: "transparent",
+      red:"#F87171",
+    },
     extend: {},
   },
   variants: {
